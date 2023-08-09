bb41c934c4096a223fbe0cfb78a48ed31d16d80b
Zotov <ZotovDendos@yandex.ru>
2023-08-09 17:42:43 +0300

    changed static

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from catalog.views import home, contacts
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('', home),
     path('contacts/', contacts)
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
