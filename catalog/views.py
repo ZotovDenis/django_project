024a272e99b34401bd5c275e25b87fe977cb97e3
Zotov <ZotovDendos@yandex.ru>
2023-08-10 16:59:33 +0300

    homework19.2 and extra task

--- catalog/views.py
+++ catalog/views.py
@@ -2,8 +2,17 @@ from django.shortcuts import render
 
 
 def home(request):
+    """Возвращает главную страницу"""
     return render(request, 'catalog/home.html')
 
 
 def contacts(request):
+    """Возвращает страницу контактов и выводит переданные пользователем данные"""
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f'Имя: {name}\n'
+              f'Телефон: {phone}\n'
+              f'Сообщение: {message}')
     return render(request, 'catalog/contacts.html')
