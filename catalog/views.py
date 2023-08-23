fc55905a1f46575e0d71d489cb51a0ba79bcd79d
Zotov <ZotovDendos@yandex.ru>
2023-08-24 00:28:04 +0300

    homework21.1

--- catalog/views.py
+++ catalog/views.py
@@ -5,32 +5,9 @@ from django.views.generic import ListView, DetailView
 from catalog.models import Product, Category
 
 
-# def home(request):
-#     """Возвращает главную страницу"""
-#     product_list = Category.objects.all()
-#     context = {
-#         'object_list': product_list,
-#         'title': 'Категории'
-#     }
-#     return render(request, 'catalog/category_list.html', context)
-
 class CategoryListView(ListView):
     model = Category
 
-
-# def contacts(request):
-#     """Возвращает страницу контактов и выводит переданные пользователем данные"""
-#     if request.method == 'POST':
-#         name = request.POST.get('name')
-#         phone = request.POST.get('phone')
-#         message = request.POST.get('message')
-#         print(f'Имя: {name}\n'
-#               f'Телефон: {phone}\n'
-#               f'Сообщение: {message}')
-#     context = {
-#         'title': 'Контакты'
-#     }
-#     return render(request, 'catalog/contacts.html', context)
 
 class ContactsView(View):
     def get(self, request):
@@ -52,15 +29,6 @@ class ContactsView(View):
         return render(request, 'catalog/contacts.html', context)
 
 
-# def products(request, pk):
-#     category_item = Category.objects.get(pk=pk)
-#     context = {
-#         'products': Product.objects.filter(category=pk),
-#         # 'category': category_item.pk,
-#         'title': f'{category_item.name}'
-#     }
-#     return render(request, 'catalog/all_products.html', context)
-
 class ProductListView(ListView):
     model = Product
     context_object_name = 'products'
@@ -77,12 +45,6 @@ class ProductListView(ListView):
         context_data['title'] = category_item.name
         return context_data
 
-
-# def product_info(request, pk):
-#     context = {
-#         'product': Product.objects.get(pk=pk)
-#     }
-#     return render(request, 'catalog/product_info.html', context)
 
 class ProductDetailView(DetailView):
     model = Product
